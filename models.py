9eaa63f4f29e93e2fa3f3b84e99063c33a9dd169
lihao <imagelihao@163.com>
2020-09-18 18:05:56 +0800

    commit models

--- models.py
+++ models.py
@@ -8,6 +8,8 @@
 """
 import numpy as np
 import os
+from sklearn.datasets import load_boston
+import matplotlib.pyplot as plt
 import tensorflow as tf
 
 model = tf.keras.Sequential()
@@ -15,3 +17,5 @@ model.add(tf.keras.layers.Dense(1, input_shape=(4,)))
 model.summary()
 model.compile(optimizer='adam', loss='mse')
 model.fit()
+
+data, target = load_boston(True)
