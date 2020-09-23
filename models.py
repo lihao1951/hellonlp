f5711d3f2c23ab65153fdc9b3fbfe4feaadf5957
lihao <imagelihao@163.com>
2020-09-23 09:24:50 +0800

    修改models

--- models.py
+++ models.py
@@ -9,13 +9,20 @@
 import numpy as np
 import os
 from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+# 导入数据
+data, target = load_boston(True)
+cols = data.shape[-1]
+x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.3)
+print('train size:%s and test size:%s' % (x_train.shape[0], x_test.shape[0]))
+
 model = tf.keras.Sequential()
-model.add(tf.keras.layers.Dense(1, input_shape=(4,)))
+model.add(tf.keras.layers.Dense(1, input_shape=(cols,)))
 model.summary()
 model.compile(optimizer='adam', loss='mse')
-model.fit()
-
-data, target = load_boston(True)
+model.fit(x_train, y_train, epochs=500, batch_size=64)
+print("ecaluate")
+model.evaluate(x_test, y_test, verbose=1)
